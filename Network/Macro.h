1c6dbaab6b5ec59628bd03e0bdf42bcfeba69730
정은성 <dmstjd0911@naver.com>
2024-05-16 22:04:25 +0900

    :sparkles: :: xstop 추가, 기본 assert사용

--- Network/Macro.h
+++ Network/Macro.h
@@ -1,12 +1,5 @@
 #pragma once
+#include <assert.h>
 
-#ifdef _DEBUG
-#define Assert(condition) if(condition){ \
-    int* a = nullptr; \
-    *a = 1; \
-        }
-#else
-    #define Assert(condition)
-#endif 
-    
-        
+#define xstop() int* exception = nullptr \
+    *exception = 1;
