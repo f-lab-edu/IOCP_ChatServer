9bef8fce1da875616d47910affd4d0b75ecf2ab4
JeongEunseong <dmstjd0911@naver.com>
2024-05-18 03:32:25 +0900

    :fire: :: latencyCheck 패킷 pop 로직 삭제

--- Chat_Client/PacketHandler.cpp
+++ Chat_Client/PacketHandler.cpp
@@ -5,10 +5,7 @@ void PacketHandler::LATENCY_CHECK_Handler(shared_ptr<Session> session, Packet* p
 {
 	shared_ptr<ServerSession> serverSession = static_pointer_cast<ServerSession>(session);
 
-	clock_t tick = 0;
-	packet->pop(tick);
-	clock_t curTick = clock();
-	serverSession->AddLatency(packet->GetPacketId(), curTick - tick);
+	serverSession->AddLatency(packet->GetPacketId(), clock() - packet->GetSendTick());
 }
 
 void PacketHandler::S2C_ENTER_ROOM_NOTIFY_Handler(shared_ptr<Session> session, Packet* packet)
